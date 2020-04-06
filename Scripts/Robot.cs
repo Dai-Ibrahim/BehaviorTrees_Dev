4653923a7d20f1c326a5bb95a87c5a6cc1b17642
Dai Ibrahim <33212069+Dai-Ibrahim@users.noreply.github.com>
2020-04-05 18:44:31 -0700

    Add files via upload

---
+++ Scripts/Robot.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : Kinematic
+{
+    public Arrive arrive;
+    public Creature creature;
+
+    void Start()
+    {
+        arrive = new Arrive();
+        arrive.character = this;
+        arrive.target = newTarget;
+
+ 
+      
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        steeringUpdate = new SteeringOutput();
+        steeringUpdate.linear = arrive.getSteering().linear;
+        base.Update();
+		if (Input.GetKeyDown(KeyCode.Return))
+        {
+              ITask behavior = ConfigureBehavior();
+			  behavior.run();
+        }
+
+    }
+
+    public ITask ConfigureBehavior()
+    {
+		List<ITask> taskList = new List<ITask>();
+
+		ITask Human = new isHuman(creature);
+		ITask HumanAndDog = new isHuman(creature);
+		ITask NotHuman = new isNotHuman(creature);
+		ITask CheckIfDogIsPresent = new isRobotDogPresent(creature);
+		ITask ShootAir = new shootAir(creature);
+		ITask ShootHuman = new shootHuman(creature);
+		ITask ShootCreature = new shootCreature(creature);
+		ITask petDog = new petDog(creature);
+		
+		//not a human, shoot the creature
+		taskList.Add(Human);
+		taskList.Add(ShootAir);
+
+		Sequence SpareTheHuman = new Sequence(taskList);
+		taskList = new List<ITask>();
+
+		taskList.Add(NotHuman);
+		taskList.Add(ShootCreature);
+
+		Sequence SaveTheHuman = new Sequence(taskList);
+		taskList = new List<ITask>();
+		
+		taskList.Add(CheckIfDogIsPresent);
+		taskList.Add(Human);
+		taskList.Add(ShootHuman);
+		taskList.Add(petDog);
+		
+		
+		Sequence SaveTheDog = new Sequence(taskList);
+		taskList = new List<ITask>();
+		
+		taskList.Add(SaveTheDog);
+		taskList.Add(SpareTheHuman);
+		taskList.Add(SaveTheHuman);
+		
+
+        Selector selector = new Selector(taskList);
+        return selector;
+      
+    }
+}
---
+++ Scripts/Task.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ITask 
+{
+	 bool run();
+}
+public class  Selector : ITask
+{
+	List<ITask> children;
+	public Selector(List<ITask> taskList)
+    {
+        children = taskList;
+    }
+	public bool run()
+	{
+		foreach (ITask c in children)
+		{
+			if(c.run())
+			{
+				return true;
+			}
+		}
+		return false;
+
+		
+	}
+	
+}
+public class  Sequence : ITask
+{
+	List<ITask> children;
+	public Sequence(List<ITask> taskList)
+    {
+        children = taskList;
+    }
+	public bool run()
+	{
+		foreach (ITask c in children)
+		{
+			if(c.run()==false)
+			{
+				return false;
+			}
+		}
+		return true;
+
+		
+	}
+	
+}
+
+public class isHuman: ITask
+{
+	Creature creature;
+	public isHuman(Creature refCreature)
+	{
+		creature = refCreature;
+	}
+	public bool run()
+	{
+		Debug.Log("Checking For Human: " + creature.isHuman);
+		return creature.isHuman;
+	}
+}
+public class isNotHuman : ITask
+{
+	Creature creature;
+	public isNotHuman(Creature refCreature)
+	{
+		creature = refCreature;
+	}
+	public bool run()
+	{
+		Debug.Log("Checking For non-human: " + !creature.isHuman);
+		return !creature.isHuman;
+	}
+}
+
+public class isRobotDogPresent : ITask
+{
+	Creature creature;
+	public isRobotDogPresent(Creature refCreature)
+	{
+		creature = refCreature;
+	}
+	public bool run()
+	{
+		Debug.Log("Checking For Robot Dog: " + creature.isDogPresent);
+		return creature.isDogPresent;
+	}
+}
+
+public class shootAir: ITask
+{
+	Creature creature;
+	public shootAir(Creature refCreature)
+	{
+		creature = refCreature;
+	}
+	public bool run()
+	{
+		Debug.Log("Shooting Air");
+		return creature.isHuman;
+	}
+}
+
+public class shootHuman : ITask
+{
+	Creature creature;
+	public shootHuman(Creature refCreature)
+	{
+		creature = refCreature;
+	}
+	public bool run()
+	{
+		Debug.Log("Shooting Human");
+		return creature.isDogPresent;
+	}
+}
+public class shootCreature: ITask
+{
+	Creature creature;
+	public shootCreature(Creature refCreature)
+	{
+		creature = refCreature;
+	}
+	public bool run()
+	{
+		Debug.Log("Shooting at Creature");
+		return !creature.isHuman;
+	}
+}
+public class petDog: ITask
+{
+	Creature creature;
+	public petDog(Creature refCreature)
+	{
+		creature = refCreature;
+	}
+	public bool run()
+	{
+		Debug.Log("petting Dog");
+		return creature.isDogPresent;
+	}
+}
+
